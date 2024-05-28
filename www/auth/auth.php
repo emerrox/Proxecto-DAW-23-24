37f7387daf7b35a60f794fc5a8c13c8aeb2b0868
emerrox <maurocordal@gmail.com>
2024-05-28 20:36:58 +0200

    home

--- www/auth/auth.php
+++ www/auth/auth.php
@@ -1,13 +1,33 @@
 <?php
-if ($_SESSION['ok']) {
-    
-    header('Location: ../inicio.php');
-    exit();
 
+// Funcion para limpiar datos de entrada
+function clean_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data); 
+    return $data;
 }
 
+// Si ya se inició sesión redirige a inicio
+if ($_SESSION['ok']) {
+    header('Location: ../home');
+    exit();
+}
+
+// Precesar formulario
 if (isset($_POST['ini'])){
-    echo 'ini';
+
+    $name = clean_input($_POST['name']);
+    $pass = clean_input($_POST['pass']);
+
+    if ($name == '' || $pass == '') {
+        header('Location: ./iniciar.php');
+        exit();
+    }
+
+    echo('bieeeeen');
+    
+    // session_start();
 }
 
 
