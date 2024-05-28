04f447b77c1e2d46c079c70043cbb2757cc7d29a
emerrox <maurocordal@gmail.com>
2024-05-28 20:16:14 +0200

    iniciar

--- www/auth/auth.php
+++ www/auth/auth.php
@@ -1,5 +1,14 @@
 <?php
-
 if ($_SESSION['ok']) {
+    
     header('Location: ../inicio.php');
+    exit();
+
 }
+
+if (isset($_POST['ini'])){
+    echo 'ini';
+}
+
+
+include_once(__DIR__ . '/iniciar.php');
---
+++ www/auth/iniciar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./auth.php" method="post">
+    <label for="name">Usuario</label>
+    <input type="text" id="name" name="name" required><br><br>
+    
+    <label for="pass">Contraseña</label>
+    <input type="password" id="pass" name="pass" required><br><br>
+    
+    <button type="submit" name="ini">Iniciar</button>
+    </form>
+</body>
+</html>
