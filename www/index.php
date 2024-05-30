8e314f392a85835f778ca4e56f6076134917d27b
emerrox <maurocordal@gmail.com>
2024-05-30 21:36:16 +0200

    ahhh asi era

--- www/index.php
+++ www/index.php
@@ -2,9 +2,9 @@
 // si hay una sesion activa redirige a home y si no redirige a auth
 
 if ($_SESSION['ok']) {
-    header('Location: /home');
+    header('Location: ./home');
     exit();
 }else{
-    header('Location: /auth');
+    header('Location: ./auth');
     exit();
 }
