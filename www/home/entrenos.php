5ad647649f8c6cac0d9a5db738eae01fd295f0bd
emerrox <maurocordal@gmail.com>
2024-06-04 21:47:37 +0200

    entrenos xd

--- www/home/entrenos.php
+++ www/home/entrenos.php
@@ -1,25 +1,20 @@
 <?php
 
-// include_once('../utils/bd.php')
-// $sql = "SELECT * FROM entrenos";
-// $resultado = $conn->query($sql);
-// $conn -> close();
+include_once('../utils/bd.php')
+$sql = "SELECT * FROM entrenos";
+$resultado = $conn->query($sql);
+$conn -> close();
 
 $entrenos = [];
+while ($fila = $resultado->fetch_assoc()) {
   $entrenos[] = [
-      'id' => 'a',
-      'title' => 'd'
+      'id' => $fila['id'],
+      'title' => $fila['nombre'],
+      'start' => $fila['hora_inicio'],
+      'end' => $fila['hora_fin'],
+      'desc' => $fila['descripcion']
   ];
-
-// while ($fila = $resultado->fetch_assoc()) {
-//   $entrenos[] = [
-//       'id' => $fila['id'],
-//       'title' => $fila['nombre'],
-//       'start' => $fila['hora_inicio'],
-//       'end' => $fila['hora_fin'],
-//       'desc' => $fila['descripcion']
-//   ];
-// }
+}
 
 $json = json_encode($entrenos);
 header('Content-Type: application/json');
